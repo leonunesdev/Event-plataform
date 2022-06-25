eb924e492cf48014239641e9b601a9efad176d40
Leandro <leonunes215@outlook.com>
2022-06-25 16:56:58 -0300

    Aula 4

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o7gf6c0zkp01xi8hrt3gmr/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorrization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
